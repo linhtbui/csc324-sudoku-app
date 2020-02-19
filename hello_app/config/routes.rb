4add8492038fc16b2c9659c85b8d2c75a9493165
Linh Bui <builinh@grinnell.edu>
2020-02-19 14:38:43 -0600

    HW4 updated

--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get '/', to: 'static_pages#home'
+
+  get '/roll', to: 'static_pages#roll'
+
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
